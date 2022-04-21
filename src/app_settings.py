410f75cbf22acb5bd575ff6c2f35575435067bca
Issashu Greybeard <issashu@icloud.com>
2022-04-22 01:09:11 +0300

    - Base window class (bare-bone) done. It is ready for inheritance and further extension. - Added app settings namespace - Added MainWindow class (no customization yet)

--- src/app_settings.py
+++ src/app_settings.py
@@ -2,6 +2,9 @@ import PySimpleGUI as Sg
 
 
 class AppSettings:
+    main_map = "/Users/issashu/Library/Mobile Documents/com~apple~CloudDocs/Coding/5eDnD_app/5eDnD_dm_tool/" \
+               "app_assets/BG_map_small.png"
+    window_size = (500,  400)
     layout = [[Sg.Text('Test window text')],
-              [Sg.InputText()]]
+              [Sg.Image(source=main_map)]]
     default_read_timeout = 300
--- src/main_client.py
+++ src/main_client.py
@@ -6,5 +6,5 @@ from app_main_window import MainWindow
 if __name__ == "__main__":
     app_gui = MainWindow()
     app_gui.set_layout(layout=app_gui.get_window_layout())
-    app_gui.create_window(title="test", icon=None, window_size=(800, 600))
+    app_gui.create_window(title="test", icon=None, window_size=AppSettings.window_size)
     app_gui.read_window(AppSettings.default_read_timeout)
